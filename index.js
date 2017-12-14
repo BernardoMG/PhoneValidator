d7cd44c79800e9bc5a59948ee80b71f370a36534
Bernardo Graça <bernardo.marquesg@gmail.com>
2017-12-14 23:49:27 +0000

    read input file into a stream

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const fs = require('fs')
+const readline = require('readline')
 const areaCodesFile = require('./config/areacodes.js')
 
 let areacodes = []
 let validNumbers = {}
+let lineReader
 
 // Check args
 if (process.argv.length < 3) {
@@ -15,4 +17,20 @@ if (process.argv.length < 3) {
 // loads all area codes from '.txt' file
 function loadAreaCodes() {
   areacodes = fs.readFileSync(areaCodesFile.file, 'utf-8').split('\n')
+  readPhoneNumbersFile(process.argv[2])
+}
+
+// read the input file into a stream
+function readPhoneNumbersFile(file) {
+  lineReader = readline.createInterface({
+    input: fs.createReadStream(file),
+    output: process.stdout,
+    terminal: true
+  });
+  lineProcess()
+}
+
+// Process each line of the input file
+function lineProcess() {
+
 }
